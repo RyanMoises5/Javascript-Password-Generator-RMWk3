09f124a778a6cbd52cef843e462be624131d5eba
Ryan M <ryan.a.moises@gmail.com>
2024-01-15 21:13:11 -0500

    finished main coding, need to add checks

--- Develop/script.js
+++ Develop/script.js
@@ -5,8 +5,15 @@ var lowerPW;
 var upperPW;
 var numberPW;
 var specialPW;
+var validate;
 var passwordLength;
 var passwordArray = [""];
+var lowerPWArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperPWArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numberPWArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialPWArray = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', '-', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~'];
+var randomIndex;
+var password = "";
 
 function characterPrompts() {
   lowerPW = window.confirm("Include lowercase letters to password?");
@@ -14,10 +21,16 @@ function characterPrompts() {
   numberPW = window.confirm("Include numbers to password?");
   specialPW = window.confirm("Include special characters to password?");
   if (!lowerPW && !upperPW && !numberPW && !specialPW) {
-    window.alert("No character type selected! Please redo character selection.");
-    prompts();
+    window.alert("No character type selected! Please redo character type selection.");
+    characterPrompts();
   }
-}
+
+  validate = window.confirm("Characters chosen to be included:\n\nLowercase Letters: " + lowerPW + "\nUppercase Letters: " + upperPW + "\nNumbers: " + numberPW + "\nSpecial Characters: " + specialPW + "\n\nIs this ok?");
+  if (validate === false) {
+    window.alert("Please redo character type selection.");
+    characterPrompts();
+    }
+  }
 characterPrompts();
 
 function passwordLengthPrompt() {
@@ -32,11 +45,37 @@ function passwordLengthPrompt() {
 }
 passwordLengthPrompt();
 
+function formulatePasswordArray() {
+  if (lowerPW == true) {
+    passwordArray = passwordArray.concat(lowerPWArray);
+  }
+  if (upperPW == true) {
+    passwordArray = passwordArray.concat(upperPWArray);
+  }
+  if (numberPW == true) {
+    passwordArray = passwordArray.concat(numberPWArray);
+  }
+  if (specialPW == true) {
+    passwordArray = passwordArray.concat(specialPWArray);
+  }
+  passwordArray.shift()
+}
+formulatePasswordArray();
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
+  password = "";
+  
+  function generatePassword() {
+    for (var i = 0; i < passwordLength; i++) {
+      randomIndex = Math.floor(Math.random() * passwordArray.length);
+      password = password + passwordArray[randomIndex];
+    }
+  }
+  generatePassword();
+
+  var passwordText = document.querySelector("#password");
   passwordText.value = password;
 
 }
